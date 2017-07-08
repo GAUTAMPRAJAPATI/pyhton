3fdfff3a9da52c130c29b5c91bd2d1b5b1eba896
GAUTAMPRAJAPATI <gautamprajapati811996@gmail.com>
2017-07-08 15:35:31 +0530

    Add files via upload

---
+++ characterinput.py
@@ -0,0 +1,4 @@
+name, age,time = input("enter your name ,age and your lucky number:")
+age= (100-age) + 2017
+statement =  "Your Good Name is:"+name , "You turn 100 on Age is: %d" %age
+print statement*time
